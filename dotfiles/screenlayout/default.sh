0faa9c85c6e1e0da20835f2ade86f2cdc1f238b7
Robert Albus <hello@robertalbus.com>
2022-07-06 23:35:06 -0600

    add display configuration

--- dotfiles/screenlayout/default.sh
+++ dotfiles/screenlayout/default.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#xrandr --output VGA-1 --off --output HDMI-1 --off --output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal
 
-xrandr --output HDMI-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --mode 2560x1440 --pos 0x0 --rotate normal #--set TearFree on
-
-#xrandr --output VGA-1 --off --output HDMI-1 --off --output HDMI-2 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1-3 --off --output DVI-D-1-1 --off
+# for the nvidia card in bigboy
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off
